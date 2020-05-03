4d82df5506d9ae852615844ae3a947bb095258f6
huhn511 <sehe89@gmail.com>
2020-05-03 12:23:03 +0200

    add iota streams

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,15 @@ use rand::prelude::*;
 use std::env;
 use std::sync::Mutex;
 
+use iota_streams::{
+    app_channels::api::tangle::{Author}
+};
+use iota_lib_rs::prelude::iota_client;
+use iota_streams::app::transport::tangle::client::SendTrytesOptions;
+use crate::author::announce::start_a_new_channel;
+use crate::author::send_message::send_signed_message;
+mod author;
+
 #[get("/drone")]
 async fn drone(req: HttpRequest, drone: web::Data<Mutex<Drone>>) -> Result<HttpResponse> {
     // println!("{:?}", req);
@@ -31,6 +40,35 @@ async fn drone(req: HttpRequest, drone: web::Data<Mutex<Drone>>) -> Result<HttpR
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
+
+    //  -------- IOTA network settings ---------
+
+    // Connect to an IOTA node
+    let mut client = iota_client::Client::new("https://nodes.comnet.thetangle.org:443");
+
+    // Change the default settings to use a lower minimum weight magnitude for the Devnet
+    let mut send_opt = SendTrytesOptions::default();
+    // default is 14
+    send_opt.min_weight_magnitude = 10;
+    send_opt.local_pow = false;
+
+    // --------------- Author -------------------
+
+    // Create a new channel
+    // REPLACE THE SECRET WITH YOUR OWN
+    let mut author = Author::new("NYAUTHORSECRET", 3, true);
+
+    let channel_address = author.channel_address().to_string();
+    println!("Channel address: {}", &channel_address);
+
+    // Send the `Announce` message
+    match start_a_new_channel(&mut author, &mut client, send_opt) {
+        Ok(()) => (),
+        Err(error) => println!("Failed with error {}", error),
+    }
+
+
+     //  -------- actix_web Web Server ---------
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
